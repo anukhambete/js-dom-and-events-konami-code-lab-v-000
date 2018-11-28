579627fb2573a7f951bf951f0d7bcf1a8a013338
Anu Khambete <anukhambete@gmail.com>
2018-11-28 04:39:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,26 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 // 37 - left arrow key 38 --up        39-right 40-down
 // up,up,down,down,left,right,left,right,a,b
-
-let index = 0;
-
-function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
-  let var = e.which;
-  const key = parseInt(e.which);
+  let index = 0;
+  document.body.addEventListener('keydown', function(e){
+  const key = parseInt(e.which || e.detail);
   if (key === code[index]) {
 
   	index++;
 
   	if(index === code.length) {
-  		alert("You did it!!");
+  		alert('Hurray!');
   		index = 0;
   	}
     } else {
       index = 0;
       }
+});
+
 }
 
-document.body.addEventListener('keydown', init);
+init();
+//document.body.addEventListener('keydown', init);
 
 //document.body.addEventListener('keydown',init);
